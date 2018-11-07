9512e8a4a0522a87eda0adf7bb986f3afa03453a
royarn <763094810@qq.com>
2018-11-07 17:14:38 +0800

    kafka props

--- src/main/java/com/royarn/goods/config/Listener.java
+++ src/main/java/com/royarn/goods/config/Listener.java
@@ -1,9 +1,14 @@
 package com.royarn.goods.config;
 
+import com.google.gson.Gson;
+import com.royarn.goods.entity.MsgInfo;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
+
+import javax.annotation.Resource;
 
 /**
  * Description:
@@ -15,9 +20,13 @@ public class Listener {
 
     private final Logger logger = LoggerFactory.getLogger(Listener.class);
 
+    @Resource
+    private MongoTemplate template;
+
     @KafkaListener(topics = {"test"})
     public void listen(ConsumerRecord<?, ?> record) {
         logger.info("kafka的key: " + record.key());
         logger.info("kafka的value: " + record.value().toString());
+        template.insert(new Gson().fromJson(record.value().toString(), MsgInfo.class));
     }
 }
---
+++ src/main/java/com/royarn/goods/config/MongoConfig.java
@@ -0,0 +1,55 @@
+package com.royarn.goods.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+/**
+ * @author lizq
+ * @Description: ${todo}
+ * @date 2018/7/12 12:50
+ */
+@Configuration
+@ConfigurationProperties(prefix = "mongodb")
+public class MongoConfig {
+
+    private String host;
+    private int port;
+    private String dataSource;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(String dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        return new MongoClient(host + ":" + port);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), dataSource);
+    }
+}
